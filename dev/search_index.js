a9c2b947ee40936755e7927fe11dd4b2638e3df4
Documenter.jl <documenter@juliadocs.github.io>
2024-04-06 00:54:10 +0000

    build based on ee8db8b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScalarWave3D","category":"page"},{"location":"#ScalarWave3D","page":"Home","title":"ScalarWave3D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScalarWave3D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScalarWave3D]","category":"page"}]
+}
